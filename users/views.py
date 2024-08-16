a42ee7de91580ef152a88f54e901e0e2f72f5016
Milen Mintchev <171292500+bulgarska@users.noreply.github.com>
2024-08-15 20:35:40 -0700

    Created authentication system  for the app so users can login and logout. Added restriction to certain pages so users must be logged in to access the page.

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 # Create your views here.
@@ -11,11 +12,17 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             # Flash message that appears for account creation confirmation and disappears
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been! You are now able to login.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+# Decorators add functionality to existing function
+# login_required user must be logged in to view page
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
 
 # messages.debug
 # messages.info
