4c4de81cda2ff6206a6204d89af80541114a814a
Milen Mintchev <171292500+bulgarska@users.noreply.github.com>
2024-08-21 15:06:36 -0700

    Implemented pagination to only display a certain amount of posts on a page at a time instead of every post in the database. Users can now see all posts made by an author by clicking on the author of a post

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 urlpatterns = [
     # parses url to check if the next element/ is present
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     # looks for the template in <app>/<model>_form.html by default
     path('post/new/', PostCreateView.as_view(), name='post-create'),
